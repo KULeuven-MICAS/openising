9ba15d3ed86134b5c8f6b5c1e26b1638a0f113ba
Sofie De Weer <sdeweer@esat.kuleuven.ac.be>
2024-10-24 11:12:51 +0200

    Finished SCA algorithm

--- sw/dummy_examples/Optimisation_algorithms/SCA.py
+++ sw/dummy_examples/Optimisation_algorithms/SCA.py
@@ -1,12 +1,27 @@
 import random
 import numpy as np
+import math
 
 def SCA(s_init, J, h, S, q_init, T_init, r_q, r_t):
     q = q_init
     T = T_init
     N = np.length(s_init)
     sigma = s_init
-
+    tau = sigma
     for s in range(S):
         for x in range(N):
-            pass
+            h[x] = np.inner(J[x,:], sigma) + h[x]
+            P = get_prob(T, h[x], q, sigma[x])
+            rand = random.randrange(0, 1)
+            if P < rand:
+                tau[x] = -sigma[x]
+        for x in range(N):
+            sigma[x] = tau[x]
+        q = q*r_q
+        T = T*r_t
+    return sigma
+
+
+def get_prob(Temp, hx, qs, sigmax):
+   x = 1/Temp*(hx*sigmax + qs)/2
+   return 1/(1+math.exp(x))
