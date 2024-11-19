54e769d1e287b67778138b9b5b60c62b200f4193
Sofie De Weer <sdeweer@esat.kuleuven.ac.be>
2024-11-19 17:23:07 +0100

    fix: numpy now used

--- ising/solvers/SCA.py
+++ ising/solvers/SCA.py
@@ -2,7 +2,7 @@ import numpy as np
 from solver import Solver
 
 class SCA(Solver):
-    def __init__(self, sigma, model, file, S, T, r_t, q, r_q, verbose):
+    def __init__(self, sigma:np.ndarray, model, file, S, T, r_t, q, r_q, verbose):
             Solver.__init__(sigma, model, file, verbose)
             self.S = S
             self.T = T
