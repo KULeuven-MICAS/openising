a85f73c75841cec5e16cc53bb2ac3b3a2837eacd
Sofie De Weer <sdeweer@esat.kuleuven.ac.be>
2024-11-22 14:00:09 +0100

    fix: solvers work with new ising model

--- ising/solvers/SB.py
+++ ising/solvers/SB.py
@@ -1,7 +1,8 @@
-from ising.model import BinaryQuadraticModel
+from ising.model.ising import IsingModel
 from ising.solvers.solver import Solver
 import numpy as np
 import pathlib
+
 
 class SB(Solver):
     """Implements discrete Simulated bifurcation as is seen in the paper of [Goto et al.](https://www.science.org/doi/10.1126/sciadv.abe7953).
@@ -10,73 +11,75 @@ class SB(Solver):
     This class inherits from the abstract Solver base class.
     """
 
-    def __init__(self, x:np.ndarray, y:np.ndarray, file:pathlib.Path, S:int, a0:float, c0:float, dt:float, at:callable):
-        self.x = x
-        self.sigma = np.sign(x)
-        self.y = y
-        self.S = S
-        self.a0 = a0
-        self.c0 = c0
-        self.dt = dt
-        self.at = at
-        self.file = file
+    def update_x(self, y, dt, a0, node):
+        return a0 * y[node] * dt
 
-    def update_x(self, node):
-        self.x[node] += self.a0*self.y[node]*self.dt
+    def update_rule(self, x, y, node):
+        x[node] = np.sign(x[node])
+        y[node] = 0.0
+        return x, y
 
-    def set_sigma(self):
-        self.sigma = np.sign(self.x)
-
-    def update_rule(self, node):
-        self.x[node] = np.sign(self.x[node])
-        self.y[node] = 0.
-
-    def solve(self, bqm:BinaryQuadraticModel):
+    def solve(self, model: IsingModel):
         pass
 
+
 class ballisticSB(SB):
-
-    def __init__(self, x, y, file, S, a0, c0, dt, at):
-        super().__init__(x, y, file, S, a0, c0, dt, at)
-
-    def solve(self, bqm:BinaryQuadraticModel):
-        tk = 0
-        N = bqm.num_variables
-        h, J = bqm.to_ising()
-        with self.open_log(self.file, bqm) as log:
-            for i in range(self.S):
+    def solve(
+        self,
+        model: IsingModel,
+        file: pathlib.Path,
+        x: np.ndarray,
+        y: np.ndarray,
+        S: int,
+        a0: float,
+        at: callable,
+        c0: float,
+        dt: float,
+    ):
+        tk = 0.0
+        N = model.num_variables
+        with self.open_log(file, model, [f"x{i}" for i in range(N)]) as log:
+            for i in range(S):
                 for j in range(N):
-                    self.y[j] += (-(self.a0 - self.at(tk))*self.x[j] + self.c0*np.dot(J[:, j], self.x) + \
-                             self.c0*h[j])*self.dt
-                    self.update_x(j)
-                    if np.abs(self.x[j]) > 1:
-                        self.update_rule(j)
-                self.set_sigma()
-                energy = bqm.eval(self.sigma)
-                log.write(tk, energy, self.sigma, self.x, self.y, self.at(tk))
-                tk += self.dt
-        return self.sigma, energy
+                    y[j] += (-(a0 - at(tk)) * x[j] + c0 * np.dot(model.J[:, j], x) + c0 * model.h[j]) * dt
+                    x[j] += self.update_x(y, dt, a0, j)
+                    if np.abs(x[j]) > 1:
+                        x, y = self.update_rule(x, y, j)
+                sample = self.set_sample(x)
+                energy = model.evaluate(sample)
+                log.write(tk, energy, sample, x)
+                tk += dt
+        return sample, energy
 
 
 class discreteSB(SB):
-
-    def __init__(self, x, y, file, S, a0, c0, dt, at):
-        super().__init__(x, y, file, S, a0, c0, dt, at)
-
-    def solve(self, bqm):
-        h, J = bqm.to_ising()
-        N = bqm.num_variables
-        tk = 0.
-        with self.open_log(self.file, bqm) as log:
-            for i in range(self.S):
+    def solve(
+        self,
+        model: IsingModel,
+        file: pathlib.Path,
+        x: np.ndarray,
+        y: np.ndarray,
+        S: int,
+        a0: float,
+        at: callable,
+        c0: float,
+        dt: float,
+    ):
+        N = model.num_variables
+        tk = 0.0
+        with self.open_log(file, model, [f'x{i}' for i in range(N)]) as log:
+            for i in range(S):
                 for j in range(N):
-                    self.y[j] += (-(self.a0 - self.at(tk))*self.x[j] + self.c0*np.inner(J[:, j], np.sign(self.x)) + \
-                            self.c0*h[j])*self.dt
-                    self.update_x()
-                    if np.abs(self.x[j]) > 1:
-                        self.update_rule(j)
-                self.set_sigma()
-                energy = bqm.eval(self.sigma)
-                tk += self.dt
-                log.write(tk, energy, self.sigma, self.x, self.y, self.at(tk))
-        return self.sigma
+                    y[j] += (
+                        -(a0 - at(tk)) * x[j]
+                        + c0 * np.inner(model.J[:, j], np.sign(x))
+                        + c0 * model.h[j]
+                    ) * dt
+                    x[j] += self.update_x(y, dt, a0, j)
+                    if np.abs(x[j]) > 1:
+                        x, y = self.update_rule(x, y, j)
+                sample = self.set_sample()
+                energy = model.evaluate(sample)
+                tk += dt
+                log.write(tk, energy, sample, x)
+        return sample, energy
--- ising/solvers/SCA.py
+++ ising/solvers/SCA.py
@@ -1,8 +1,9 @@
 import numpy as np
 from ising.solvers.solver import Solver
-from ising.model import BinaryQuadraticModel
+from ising.model.ising import IsingModel
 import pathlib
 import random
+
 
 class SCA(Solver):
     """Implementation of the Stochastic Cellular Automata (SCA) annealing algorithm of the
@@ -10,63 +11,49 @@ class SCA(Solver):
 
     Inherits from the abstract Solver base class.
     """
+    def change_hyperparam(self, param: float, rate: float) -> float:
+        """Changes hyperparameters according to update rule."""
+        return param * rate
 
-    def __init__(self, sigma:np.ndarray, file:pathlib.Path, S:int, T:float, r_t:float, q:float, r_q:float):
-        """Initializes object.
-
-        Args:
-            sigma (np.ndarray): initial sample
-            file (pathlib.Path): full path to the logger file
-            S (int): total amount of iterations
-            T (float): initial temperature
-            r_t (float): temperature decrease rate
-            q (float): initial penalty
-            r_q (float): penalty increase rate
-        """
-        self.sigma = sigma
-        self.S = S
-        self.T = T
-        self.r_t = r_t
-        self.q = q
-        self.r_q = r_q
-        self.file = file
-        self.hs = np.ndarray()
-
-    def change_hyperparams(self)->None:
-        """Changes hyperparameters according to update rule.
-        """
-        self.T = self.r_t*self.T
-        self.q = self.r_q*self.q
-
-
-    def solve(self, bqm:BinaryQuadraticModel, seed:int):
-        N = bqm.num_variables
-        h, J = bqm.to_ising()
-        self.hs = np.copy(h)
+    def solve(
+        self,
+        model: IsingModel,
+        file: pathlib.Path,
+        seed: int,
+        sample: np.ndarray,
+        S: int,
+        T: float,
+        r_t: float,
+        q: float,
+        r_q: float,
+    ):
+        N = model.num_variables
+        hs = np.copy(model.h)
         flipped_states = []
         random.seed(seed)
-        with self.open_log(bqm) as log:
-            for s in range(self.S):
+        with self.open_log(file, model) as log:
+            for s in range(S):
                 for x in range(N):
-                    self.hs[x] += np.dot(J[x, :], self.sigma)
-                    P = self.get_prob(x)
+                    hs[x] += np.dot(model.J[x, :], sample)
+                    P = self.get_prob(hs[x], sample[x], q, T)
                     rand = random.random()
                     if P < rand:
                         flipped_states.append(x)
                 for x in flipped_states:
-                    self.change_node(x)
-                self.change_hyperparams()
+                    sample = self.change_node(sample, x)
+                T = self.change_hyperparam(T, r_t)
+                q = self.change_hyperparam(q, r_q)
                 flipped_states = []
-                energy = bqm.eval(self.sigma)
-                log.write(s, energy, self.sigma, self.T, self.q)
+                energy = model.evaluate(sample)
+                log.write(s, energy, sample)
 
         return self.sigma, energy
 
-    def get_prob(self, x:int):
-        val = self.hs[x] * self.sigma[x] + self.q
-        if -2 * self.T < val < 2 * self.T:
-            return val / (4*self.T) + 0.5
-        elif val > 2 * self.T:
-            return 1.
+    def get_prob(self, hsx, samplex, q, T):
+        val = hsx * samplex + q
+        if -2 * T < val < 2 * T:
+            return val / (4 * T) + 0.5
+        elif val > 2 * T:
+            return 1.0
         else:
-            return 0.
+            return 0.0
