68bb53ff9576233bd640abc9e70d799aca818338
Sofie De Weer <71072920+sofiedeweer@users.noreply.github.com>
2024-11-29 17:56:03 +0100

    fix: BRIM and SB work with new logger. (#28)
    Co-authored-by: Sofie De Weer <sdeweer@gliese.esat.kuleuven.be>

--- ising/solvers/BRIM.py
+++ ising/solvers/BRIM.py
@@ -1,10 +1,12 @@
-from ising.solvers.solver import Solver
-from ising.model.ising import IsingModel
 import numpy as np
 import pathlib
 
+from ising.solvers.base import SolverBase
+from ising.model.ising import IsingModel
+from utils.HDF5Logger import HDF5Logger
 
-class BRIM(Solver):
+
+class BRIM(SolverBase):
 
     def k(self, kmax, kmin, cycle_duration, t):
         return kmax if int(t // (cycle_duration / 2)) % 2 == 0 else kmin
@@ -12,19 +14,52 @@ class BRIM(Solver):
     def solve(
         self,
         model: IsingModel,
-        file: pathlib.Path,
         v: np.ndarray,
-        S: int,
+        num_iterations: int,
         dt: float,
         kmin: float,
         kmax: float,
         C: float,
         G: float,
-    ):
+        file: pathlib.Path|None = None,
+    ) -> tuple[np.ndarray, float]:
+        """Simulates the BLIM dynamics by integrating the Lyapunov equation through time with the RK4 method.
+
+        Args:
+            model (IsingModel): the model of which the optimum needs to be found.
+            v (np.ndarray): initial voltages of the nodes
+            num_iterations (int): amount of iterations that need to be simulated
+            dt (float): time step.
+            kmin (float): minimal gain of the latch
+            kmax (float): maximum gain of the latch
+            C (float): capacitor parameter.
+            G (float): resistance parameter.
+            file (pathlib.Path, None, Optional): absolute path to which data will be logged. If 'None',
+                                                 nothing is logged.
+
+        Returns:
+            (sample, energy) tuple[np.ndarray, float]: optimal sample and energy.
+        """
         N = model.num_variables
         tk = 0.0
-        cycle_duration = dt * S / 10
+        cycle_duration = dt * num_iterations / 10
 
+        schema = {
+            "time": float,
+            "energy": np.float32,
+            "state": (np.int8, (N,)),
+            "voltages": (np.float32, (N,))
+        }
+
+        metadata = {
+            "solver": "BLIM",
+            "initial_state": np.sign(v),
+            "num_iterations": num_iterations,
+            "time_step": dt,
+            "C": C,
+            "G": G,
+            "(kmin, kmax)": (kmin, kmax)
+        }
         def dvdt(t, v):
             V = np.array([v] * N)
             k = self.k(kmin, kmax, cycle_duration, t)
@@ -36,8 +71,9 @@ class BRIM(Solver):
             dv = np.where(np.all(np.array([dv < 0.0, v <= -1.0]), 0), np.zeros((N,)), dv)
             return dv
 
-        with self.open_log(file, model, [f'v{i}' for i in range(N)]) as log:
-            for i in range(self.S):
+        with HDF5Logger(file, schema) as log:
+            log.write_metadata(**metadata)
+            for i in range(num_iterations):
                 k1 = dvdt(tk, v)
                 k2 = dvdt(tk + dt / 2, v + dt / 2 * k1)
                 k3 = dvdt(tk + dt / 2, v + dt / 2 * k2)
@@ -46,6 +82,7 @@ class BRIM(Solver):
                 v += dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
                 sample = np.sign(v)
                 energy = model.evaluate(sample)
+                log.log(time=tk, energy=energy, state=sample, voltages=v)
                 tk += dt
-                log.write(tk, energy, sample, v)
+            log.write_metadata(solution_State=sample, solution_energy=energy)
         return sample, energy
--- ising/solvers/SB.py
+++ ising/solvers/SB.py
@@ -1,10 +1,13 @@
-from ising.model.ising import IsingModel
-from ising.solvers.solver import Solver
 import numpy as np
 import pathlib
+from abc import abstractmethod
+
+from ising.model.ising import IsingModel
+from ising.solvers.base import SolverBase
+from ising.utils.HDF5Logger import HDF5Logger
 
 
-class SB(Solver):
+class SB(SolverBase):
     """Implements discrete Simulated bifurcation as is seen in the paper of [Goto et al.](https://www.science.org/doi/10.1126/sciadv.abe7953).
     This implementation is an improved version of the classical adiabatic Simulated Bifurcation algorithm.
 
@@ -19,6 +22,7 @@ class SB(Solver):
         y[node] = 0.0
         return x, y
 
+    @abstractmethod
     def solve(self, model: IsingModel):
         pass
 
@@ -27,28 +31,67 @@ class ballisticSB(SB):
     def solve(
         self,
         model: IsingModel,
-        file: pathlib.Path,
         x: np.ndarray,
         y: np.ndarray,
-        S: int,
-        a0: float,
+        num_iterations: int,
         at: callable,
         c0: float,
         dt: float,
-    ):
+        a0: float=1.,
+        file: pathlib.Path | None = None,
+    ) -> tuple[np.ndarray, float]:
+        """Performs the ballistic Simulated Bifurcation algorithm first proposed by [Goto et al.](https://www.science.org/doi/10.1126/sciadv.abe7953).
+        This variation of Simulated Bifurcation introduces perfectly inelastic walls at |x_i| = 1
+        to reduce analog errors.
+
+        Args:
+            model (IsingModel): the model of which the optimum needs to be found.
+            x (np.ndarray): the initial position of the nonlinear oscillators.
+            y (np.ndarray): the initial momenta of the nonlinear oscillators.
+            num_iterations (int): amount of iterations that needs to be performed.
+            a0 (float, Optional): hyperparameter. Defaults to 1.
+            at (callable): changing hyperparameter that induces the bifurcation.
+            c0 (float): hyperparameter.
+            dt (float): time step.
+            file (pathlib.Path, None, Optional): full path to which data will be logged. If 'None',
+                                                 no logging is performed
+
+        Returns:
+            sample, energy (tuple[np.ndarray, float]): optimal solution and energy
+        """
         tk = 0.0
         N = model.num_variables
-        with self.open_log(file, model, [f"x{i}" for i in range(N)]) as log:
-            for i in range(S):
+
+        schema = {
+            "time": float,
+            "energy": np.float32,
+            "state": (np.int8, (N,)),
+            "positions": (np.int32, (N,)),
+            "momenta": (np.int32, (N,)),
+            "at": np.float32,
+        }
+
+        metadata = {
+            "solver": "ballistic_simulated_bifurcation",
+            "time_step": dt,
+            "a0": a0,
+            "c0": c0,
+            "num_iterations": num_iterations,
+        }
+        with HDF5Logger(file, schema) as log:
+            log.write_metadata(**metadata)
+            for i in range(num_iterations):
+                atk = at(tk)
                 for j in range(N):
-                    y[j] += (-(a0 - at(tk)) * x[j] + c0 * np.dot(model.J[:, j], x) + c0 * model.h[j]) * dt
+                    y[j] += (-(a0 - atk) * x[j] + c0 * np.dot(model.J[:, j], x) + c0 * model.h[j]) * dt
                     x[j] += self.update_x(y, dt, a0, j)
                     if np.abs(x[j]) > 1:
                         x, y = self.update_rule(x, y, j)
                 sample = np.sign(x)
                 energy = model.evaluate(sample)
-                log.write(tk, energy, sample, x)
+                log.log(time=tk, energy=energy, state=sample, positions=x, momenta=y, at=at(tk))
                 tk += dt
+            log.write_metadata(solution_state=sample, solution_energy=energy)
         return sample, energy
 
 
@@ -56,30 +99,65 @@ class discreteSB(SB):
     def solve(
         self,
         model: IsingModel,
-        file: pathlib.Path,
         x: np.ndarray,
         y: np.ndarray,
-        S: int,
-        a0: float,
+        num_iterations: int,
         at: callable,
         c0: float,
         dt: float,
-    ):
+        a0: float=1.,
+        file: pathlib.Path|None=None,
+    )-> tuple[np.ndarray, float]:
+        """Performs the discrete Simulated Bifurcation algorithm first proposed by [Goto et al.](https://www.science.org/doi/10.1126/sciadv.abe7953).
+        This variation of Simulated Bifurcation discretizes the positions x_i at all times to reduce analog errors.
+
+        Args:
+            model (IsingModel): the model of which the optimum needs to be found.
+            x (np.ndarray): the initial position of the nonlinear oscillators.
+            y (np.ndarray): the initial momenta of the nonlinear oscillators.
+            num_iterations (int): amount of iterations that needs to be performed.
+            a0 (float, Optional): hyperparameter. Defaults to 1.
+            at (callable): changing hyperparameter that induces the bifurcation.
+            c0 (float): hyperparameter.
+            dt (float): time step.
+            file (pathlib.Path, None, Optional): full path to which data will be logged. If 'None',
+                                                 no logging is performed
+
+        Returns:
+            sample, energy (tuple[np.ndarray, float]): optimal solution and energy
+        """
         N = model.num_variables
         tk = 0.0
-        with self.open_log(file, model, [f'x{i}' for i in range(N)]) as log:
-            for i in range(S):
+
+        schema = {
+            "time": float,
+            "energy": np.float32,
+            "state": (np.int8, (N,)),
+            "positions": (np.int32, (N,)),
+            "momenta": (np.int32, (N,)),
+            "at": np.float32,
+        }
+
+        metadata = {
+            "solver": "discrete_simulated_bifurcation",
+            "time_step": dt,
+            "a0": a0,
+            "c0": c0,
+            "num_iterations": num_iterations,
+        }
+
+        with HDF5Logger(file, schema) as log:
+            log.write_metadata(metadata)
+            for i in range(num_iterations):
+                atk = at(tk)
                 for j in range(N):
-                    y[j] += (
-                        -(a0 - at(tk)) * x[j]
-                        + c0 * np.inner(model.J[:, j], np.sign(x))
-                        + c0 * model.h[j]
-                    ) * dt
+                    y[j] += (-(a0 - atk) * x[j] + c0 * np.inner(model.J[:, j], np.sign(x)) + c0 * model.h[j]) * dt
                     x[j] += self.update_x(y, dt, a0, j)
                     if np.abs(x[j]) > 1:
                         x, y = self.update_rule(x, y, j)
-                sample = np.sign()  # TODO: I think this is broken
+                sample = np.sign(x)
                 energy = model.evaluate(sample)
+                log.log(time=tk, energy=energy, state=sample, positions=x, momenta=y, at=atk)
                 tk += dt
-                log.write(tk, energy, sample, x)
+            log.write_metadata(solution_state=sample, solution_energy=energy)
         return sample, energy
