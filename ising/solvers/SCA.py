95704511cc3533fd4d5dc11b5dd35c7704e3dd32
Sofie De Weer <71072920+sofiedeweer@users.noreply.github.com>
2024-12-02 11:37:40 +0100

    Sofie/optim algo (#29)
    * fix: BRIM and SB work with new logger.
    
    * fix: SCA works with HDF5Logger now.
    
    ---------
    
    Co-authored-by: Sofie De Weer <sdeweer@gliese.esat.kuleuven.be>

--- ising/solvers/SCA.py
+++ ising/solvers/SCA.py
@@ -1,16 +1,13 @@
 import numpy as np
-from ising.solvers.solver import Solver
-from ising.model.ising import IsingModel
 import pathlib
 import random
+import time
 
+from ising.solvers.base import SolverBase
+from ising.model.ising import IsingModel
+from ising.utils.HDF5Logger import HDF5Logger
 
-class SCA(Solver):
-    """Implementation of the Stochastic Cellular Automata (SCA) annealing algorithm of the
-    [STATICA](https://ieeexplore.ieee.org/document/9222223/?arnumber=9222223) paper
-
-    Inherits from the abstract Solver base class.
-    """
+class SCA(SolverBase):
     def change_hyperparam(self, param: float, rate: float) -> float:
         """Changes hyperparameters according to update rule."""
         return param * rate
@@ -18,21 +15,61 @@ class SCA(Solver):
     def solve(
         self,
         model: IsingModel,
-        file: pathlib.Path,
-        seed: int,
         sample: np.ndarray,
-        S: int,
+        num_iterations: int,
         T: float,
         r_t: float,
         q: float,
         r_q: float,
+        seed: int|None = None,
+        file: pathlib.Path|None = None,
     ):
+        """Implementation of the Stochastic Cellular Automata (SCA) annealing algorithm of the
+        [STATICA](https://ieeexplore.ieee.org/document/9222223/?arnumber=9222223) paper
+
+        Args:
+            model (IsingModel): instance of the Ising model that needs to be optimised.
+            sample (np.ndarray): initial state of the Ising model.
+            num_iterations (int): total amount of iterations which the solver needs to perform.
+            T (float): temperature needed for the annealing process
+            r_t (float): decrease rate of the temperature
+            q (float): penalty parameter to ensure the copy states are equivalent to the real states.
+            r_q (float): increase rate of the penalty parameter
+            seed (int, None, optional): seed to generate random numbers. Important for reproducibility.
+                                        Defaults to None.
+            file (pathlib.Path, None, optional): absolute path to the logger file for logging the optimisation process.
+                                                 If 'None', no logging is performed.
+
+        Returns:
+            sample, energy (tuple[np.ndarray, float]): final state and energy of the optimisation process.
+        """
         N = model.num_variables
         hs = np.copy(model.h)
         flipped_states = []
+
+        if seed is None:
+            seed = int(time.time() * 1000)
         random.seed(seed)
-        with self.open_log(file, model) as log:
-            for s in range(S):
+
+        schema = {
+            "energy": np.float32,
+            "state": (np.int8, (N,))
+        }
+
+        metadata = {
+            "solver": "Stochastic_cellular_automata_annealing",
+            "initial_temp": T,
+            "cooling_rate": r_t,
+            "initial_penalty": q,
+            "penalty_increase": r_q,
+            "seed": seed,
+            "num_iterations": num_iterations,
+            "initial_state": sample
+        }
+        with HDF5Logger(file, schema) as log:
+            log.write_metadata(metadata)
+
+            for s in range(num_iterations):
                 for x in range(N):
                     hs[x] += np.dot(model.J[x, :], sample)
                     P = self.get_prob(hs[x], sample[x], q, T)
@@ -40,16 +77,32 @@ class SCA(Solver):
                     if P < rand:
                         flipped_states.append(x)
                 for x in flipped_states:
-                    sample = self.change_node(sample, x)
+                    sample[x] = -sample[x]
+                energy = model.evaluate(sample)
+                log.log(energy=energy, state=sample)
+
+
                 T = self.change_hyperparam(T, r_t)
                 q = self.change_hyperparam(q, r_q)
                 flipped_states = []
-                energy = model.evaluate(sample)
-                log.write(s, energy, sample)
 
-        return self.sigma, energy
+            log.write_metadata(solution_state=sample, solution_energy=energy)
 
-    def get_prob(self, hsx, samplex, q, T):
+        return sample, energy
+
+    def get_prob(self, hsx:float, samplex:int, q:float, T:float)->float:
+        """Calculates the probability of changing the value of a certain node
+           according to SCA annealing process.
+
+        Args:
+            hsx (float): local field influence on the node.
+            samplex (int): node
+            q (float): penalty parameter
+            T (float): temperature
+
+        Returns:
+            probability (float): probability of accepting the change.
+        """
         val = hsx * samplex + q
         if -2 * T < val < 2 * T:
             return val / (4 * T) + 0.5
