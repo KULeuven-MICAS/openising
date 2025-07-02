10f0cdf5c8ba6e99e0b20f34e77187f1aeea71c3
Sofie De Weer <sofie.deweer@esat.kuleuven.be>
2025-07-02 10:14:28 +0200

    chore: better documentation for parsers

--- ising/benchmarks/parsers/ATSP.py
+++ ising/benchmarks/parsers/ATSP.py
@@ -6,21 +6,23 @@ from ising.flow import TOP
 from ising.utils.parser import get_optim_value
 
 def ATSP_parser(benchmark:pathlib.Path|str) -> tuple[nx.DiGraph, float]:
-    """creates a networkx instance fromthe given benchmark.
+    """Creates a networkx instance from the given benchmark. It is important that the benchmark originates from [here](http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/).
 
     Args:
         benchmark (pathlib.Path | str): full path to the benchmark file
 
     Returns:
-        G (nx.DiGraph): a directed graph that originated from the given benchmark.
+        G,best_found (tuple[nx.DiGraph, float]): a directed graph that originated from the given benchmark and the best
+                                                 found solution energy.
     """
     if not benchmark.exists():
-        print("Benchmark does not exist")
-        return None
+        raise FileNotFoundError(f"The benchmark file {benchmark} does not exist.")
     benchmark = str(benchmark)
     name = benchmark.split("/")[-1].split(".")[0]
+
+    # Load the benchmark with the tsplib95 library.
     problem = tsplib95.load(benchmark)
-    G = problem.get_graph()
-    G.name = name
+    graph = problem.get_graph()
+    graph.name = name
     best_found = get_optim_value(benchmark, TOP / "ising/benchmarks/ATSP/optimal_energy.txt")
-    return G, best_found
+    return graph, best_found
--- ising/benchmarks/parsers/TSP.py
+++ ising/benchmarks/parsers/TSP.py
@@ -7,18 +7,20 @@ from ising.utils.parser import get_optim_value
 
 def TSP_parser(benchmark:pathlib.Path)->tuple[nx.DiGraph, float]:
     """Creates a graph from the given benchmark. With this graph a TSP problem can be generated.
-    It is important to note that only TSP benchmarks can be used
+    It is important to note that only TSP benchmarks can be used from [here](http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/).
 
     Args:
         benchmark (pathlib.Path): the absolute path to the benchmark file.
 
     Returns:
-        tuple[nx.DiGraph, float]: a tuple containing the graph and best found energy.
+        graph,best_found tuple[nx.DiGraph, float]: a tuple containing the graph and best found energy.
     """
+    # Ensure the benchmark exists
     if not benchmark.exists():
-        print("Benchmark does not exist")
-        return None
+        raise FileNotFoundError(f"The benchmark file {benchmark} does not exist.")
     benchmark = str(benchmark)
+
+    # Benchmark is loaded using tsplib95, that efficiently parses the benchmark file.
     problem = tsplib95.load(benchmark)
     graph = problem.get_graph()
     graph.name = benchmark.split("/")[-1].split(".")[0]
