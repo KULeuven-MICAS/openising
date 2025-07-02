6519c0feaa6d38571334aa15363a9e76564ba856
Sofie De Weer <sofie.deweer@esat.kuleuven.be>
2025-07-02 10:14:28 +0200

    chore: better documentation for parsers

--- ising/benchmarks/parsers/G.py
+++ ising/benchmarks/parsers/G.py
@@ -5,26 +5,35 @@ from ising.flow import TOP
 from ising.utils.parser import get_optim_value
 
 def G_parser(benchmark: pathlib.Path | str):
-    """Creates undirected graph from G benchmark
+    """Creates undirected graph from G benchmark. All the benchmark files should be in the same format, which is:
+
+    ``node_i node_j weight_ij``
+
+    And the first line holds the amount of nodes and edges in the problem.
 
     Args:
         benchmark (pathlib.Path | str): benchmark that needs to be generated
 
     Returns:
-        G (nx.Graph): graph generated from the benchmark
-        best_found (float): best found cut value.
+        G,best_found (tuple[nx.Graph, float]): graph generated from the benchmark and the best found cut value.
     """
+    # Make sure we keep track of where we are in the file
     data = False
     name = str(benchmark).split("/")[-1].split(".")[0]
+
+    # Initialize the graph
     G = nx.Graph(name=name)
     with benchmark.open() as f:
         for line in f:
             if not data:
+                # When the data is not yet read, we are at the first line of the file.
+                # We can read the amount of nodes and put the data part to True.
                 row = line.split()
                 N = int(row[0])
                 G.add_nodes_from(list(range(N)))
                 data = True
             else:
+                # Every line holds the first node, the second node and the weight of the edge.
                 line = line.split()
                 u = int(line[0]) - 1
                 v = int(line[1]) - 1
