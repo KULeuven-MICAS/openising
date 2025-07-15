030541f5d105eb4043579c801502b2f6fc1e4d5b
Sofie De Weer <sofie.deweer@esat.kuleuven.be>
2025-07-15 10:32:43 +0200

    fix: removed old flipping strategy

--- ising/solvers/Multiplicative.py
+++ ising/solvers/Multiplicative.py
@@ -4,7 +4,7 @@ import time
 from numpy.random import MT19937, Generator
 from collections.abc import Callable
 
-from ising.flow import LOGGER
+# from ising.flow import LOGGER
 from ising.solvers.base import SolverBase
 from ising.stages.model.ising import IsingModel
 from ising.utils.HDF5Logger import HDF5Logger
@@ -34,7 +34,7 @@ class Multiplicative(SolverBase):
         self.tot_sfs += np.sum(self.chosen_nodes)
         self.p += self.scale * self.sf_freq
 
-    def set_params(self, resistance: float, capacitance: float, mu_param: float, flipping: bool):
+    def set_params(self, resistance: float, capacitance: float, mu_param: float):
         """Set the parameters for the solver.
 
         Args:
@@ -45,8 +45,6 @@ class Multiplicative(SolverBase):
         self.resistance = resistance
         self.capacitance = capacitance
         self.mu_param = mu_param
-        self.flip_resistance = resistance / (1e2 * 128)
-        # self.flipping = flipping
 
     def set_spinflip(self, N: int, num_iterations: int, initial_prob: float, seed: int, flipping_freq: int):
         self.flip_value = np.zeros((N + int(self.bias),))
@@ -63,49 +61,15 @@ class Multiplicative(SolverBase):
         self.generator = Generator(MT19937(seed=seed))
         self.rng_: Callable = lambda size: self.generator.uniform(0, 1, size)
 
-    def dvdt_flip(self, t: float, vt: np.ndarray, coupling: np.ndarray):
-        """Differential equations for the multiplicative BRIM model.
-
-        Args:
-            t (float): time
-            vt (np.ndarray): current voltages
-            coupling (np.ndarray): coupling matrix J
-
-        Returns:
-            dv (np.ndarray): the change of the voltages
-        """
-
-        # set bias node to 1.
-        if self.bias:
-            vt[-1] = 1.0
-
-        # Buffering
-        c = 1 / np.abs(vt)
-
-        # ZIV diode
-        z = vt / self.resistance * (vt - 1) * (vt + 1) * self.mu_param
-
-        # Flipping changes
-        flip = np.where(self.chosen_nodes, (self.flip_value - vt), 0.0)
-
-        # Compute the voltage change dv
-        dv = 1 / self.capacitance * (np.dot(coupling, c * vt) - z + flip / self.flip_resistance)
-
-        # Ensure the voltages stay in the range [-1, 1]
-        cond1 = (dv > 0) & (vt > 1)
-        cond2 = (dv < 0) & (vt < -1)
-        dv *= np.where(cond1 | cond2, 0.0, 1.0)
-
-        # Ensure the bias node does not change
-        if self.bias:
-            dv[-1] = 0.0
-        return dv
+    def mosfet(self, voltage:np.ndarray ):
+        pass
 
     def dvdt(
         self,
         t: float,
         vt: np.ndarray,
         coupling: np.ndarray,
+        frozen_nodes: np.ndarray | None = None,
     ):
         """Differential equations for the multiplicative BRIM model when flipping is involved.
 
@@ -122,19 +86,16 @@ class Multiplicative(SolverBase):
         if self.bias:
             vt[-1] = 1.0
 
-        # Buffering
-        # c = np.where(np.abs(vt) > 1., 1 / np.abs(vt), vt)
-
-        # ZIV diode
-        z = vt / self.resistance * (vt - 1) * (vt + 1) * self.mu_param
-
         # Compute the voltage change dv
-        dv = 1 / self.capacitance * (np.dot(coupling,  np.sign(vt)) - z)
+        dv = 1 / self.capacitance * (np.dot(coupling,  np.sign(vt)))
 
         # Ensure the voltages stay in the range [-1, 1]
-        cond1 = (dv > 0) & (vt > 0)
-        cond2 = (dv < 0) & (vt < 0)
-        dv *= np.where(cond1 | cond2, (1-vt**2), 1.0)
+        cond1 = (dv > 0) & (vt >= 1)
+        cond2 = (dv < 0) & (vt <= -1)
+        dv *= np.where(cond1 | cond2, 0.0, 1.0)
+
+        if frozen_nodes is not None:
+            dv[frozen_nodes] = 0.0
 
         # Ensure the bias node does not change
         if self.bias:
@@ -164,10 +125,7 @@ class Multiplicative(SolverBase):
         end_temp_cont: float = 0.05,
         stop_criterion: float = 1e-8,
         mu_param: float = -3.55,
-        flipping: bool = False,
-        flipping_freq: float = 10000,
-        flipping_prob: float = 0.001799,
-        flipping_time: float = 1e-3,
+        frozen_nodes: np.ndarray | None = None,
         file: pathlib.Path | None = None,
     ) -> tuple[np.ndarray, float]:
         """Solves the given problem using a multiplicative coupling scheme.
@@ -187,9 +145,6 @@ class Multiplicative(SolverBase):
                                               too much anymore. Defaults to 1e-8.
             coupling_annealing (bool, optional): whether to anneal the coupling matrix. Defaults to False.
             mu_param (float, optional): the mu parameter for the ZIV diode. Defaults to -3.55.
-            flipping (bool, optional): whether to use the spin flip method. Defaults to False.
-            flipping_freq (int, optional): the frequency of the spin flip method in Hertz. Defaults to 10 kHz.
-            flipping_prob (float, optional): the probability of the spin flip method. Defaults to 0.001799.
             file (pathlib.Path, None, optional): the path to the logfile. Defaults to None.
 
         Returns:
@@ -198,7 +153,7 @@ class Multiplicative(SolverBase):
         # Set up the time evaluations
         tend = dtMult * num_iterations
 
-        self.set_params(resistance, capacitance, mu_param, flipping)
+        self.set_params(resistance, capacitance, mu_param)
 
         # Transform the model to one with no h and mean variance of J
         if np.linalg.norm(model.h) >= 1e-10:
@@ -228,12 +183,6 @@ class Multiplicative(SolverBase):
         # Schema for logging
         schema = {"time_clock": float, "energy": np.float32, "state": (np.int8, (N,)), "voltages": (np.float32, (N,))}
 
-        # Set up the spin flipping
-        # self.set_spinflip(N, num_iterations, flipping_prob, seed, flipping_freq)
-        # flip_period = 1/(flipping_freq*dtMult)
-        # flip_iter = np.arange(0, num_iterations, flip_period)
-        # flip_iter = np.delete(flip_iter, 0)
-
         with HDF5Logger(file, schema) as log:
             self.log_metadata(
                 logger=log,
@@ -255,50 +204,11 @@ class Multiplicative(SolverBase):
             energy = model.evaluate(np.sign(initial_state))
 
             log.log(time_clock=0.0, energy=energy, state=np.sign(initial_state), voltages=initial_state)
-            while i < num_iterations and max_change > stop_criterion:
+            while i < num_iterations or max_change <= stop_criterion:
                 tk = i * dtMult
-                # if flipping and i in flip_iter:
-                #     self.do_spinflip(previous_voltages)
-                #     dt_flip = dtMult * flipping_time
-                #     t_flip = tk
-                #     just_flipped = True
-                #     for j in range(int(flipping_time / dt_flip)):
-                #         k1 = self.dvdt_flip(tk, previous_voltages, J)
-                #         k2 = self.dvdt_flip(tk + dt_flip, previous_voltages + dt_flip * k1, J)
-                #         k3 = self.dvdt_flip(tk + 1 / 2 * dt_flip, previous_voltages + dt_flip / 4 * (k1 + k2), J)
-
-                #         # Add noise and update the voltages
-                #         noise = self.noise(Temp, previous_voltages)
-                #         new_voltages = previous_voltages + dt_flip / 6.0 * (k1 + k2 + 4.0 * k3) + noise
-                #         t_flip += dt_flip
-                #         if t_flip >= tk + dtMult:
-                #             tk += dtMult
-                #             i += 1
-                #             Temp *= cooling_rate
-
-                #             energy = model.evaluate(np.sign(new_voltages[:N]))
-                #             log.log(
-                #                 time_clock=tk, energy=energy, state=np.sign(new_voltages[:N]),
-                #                 voltages=new_voltages[:N]
-                #             )
-                #         previous_voltages = np.copy(new_voltages)
-
-                #     if t_flip < tk + dtMult:
-                #         dt = tk + dtMult - t_flip
-                #         i += 1
-                #         k1 = dt * self.dvdt(tk, previous_voltages, J)
-                #         k2 = dt * self.dvdt(tk + dtMult, previous_voltages + k1, J)
-                #         k3 = dt * self.dvdt(tk + 1 / 2 * dtMult, previous_voltages + 1 / 4 * (k1 + k2), J)
-
-                #         noise = self.noise(Temp, previous_voltages)
-                #         new_voltages = previous_voltages + 1.0 / 6.0 * (k1 + k2 + 4.0 * k3) + noise
-                #         Temp *= cooling_rate
-                #         tk += dtMult
-                # else:
-                just_flipped = False
-                k1 = dtMult * self.dvdt(tk, previous_voltages, J)
-                k2 = dtMult * self.dvdt(tk + dtMult, previous_voltages + k1, J)
-                k3 = dtMult * self.dvdt(tk + 1 / 2 * dtMult, previous_voltages + 1 / 4 * (k1 + k2), J)
+                k1 = dtMult * self.dvdt(tk, previous_voltages, J, frozen_nodes)
+                k2 = dtMult * self.dvdt(tk + dtMult, previous_voltages + k1, J, frozen_nodes)
+                k3 = dtMult * self.dvdt(tk + 1 / 2 * dtMult, previous_voltages + 1 / 4 * (k1 + k2), J, frozen_nodes)
 
                 noise = self.noise(Temp, previous_voltages)
                 new_voltages = previous_voltages + 1.0 / 6.0 * (k1 + k2 + 4.0 * k3) + noise
@@ -315,17 +225,17 @@ class Multiplicative(SolverBase):
                 #     LOGGER.info(f"Iteration {i} - time {tk:.2e} - energy {energy:.2f} - total flips {self.tot_sfs}")
 
                 # Update the criterion changes
-                if i > 0 and not just_flipped:
+                if i > 0:
                     max_change = np.linalg.norm(new_voltages - previous_voltages, ord=np.inf) / np.linalg.norm(
                         previous_voltages, ord=np.inf
                     )
                 previous_voltages = np.copy(new_voltages)
-            LOGGER.info(f"Done at iteration {i} with energy {energy:.2f}")
+            # LOGGER.info(f"Done at iteration {i} with energy {energy:.2f}")
             # Make sure to log to the last iteration if the stop criterion is reached
-            if max_change < stop_criterion:
-                for j in range(i, num_iterations):
-                    tk += dtMult
-                    log.log(time_clock=tk, energy=energy, state=sample, voltages=new_voltages[:N])
+            # if max_change < stop_criterion:
+            #     for j in range(i, num_iterations):
+            #         tk += dtMult
+            #         log.log(time_clock=tk, energy=energy, state=sample, voltages=new_voltages[:N])
 
             log.write_metadata(solution_state=sample, solution_energy=energy, total_time=tend)
         return sample, energy
