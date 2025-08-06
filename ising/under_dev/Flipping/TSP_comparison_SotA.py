fix: added frozen gradient and best parameters for flipping benchmarks

ising/under_dev/Flipping/TSP_comparison_SotA.py
@@ -1,46 +1,73 @@
 import numpy as np
 import multiprocessing as mp
 from functools import partial
+import os
 
+from ising.stages.model.ising import IsingModel
 from ising.under_dev.Flipping.new_strategy import do_flipping
 from ising.flow import TOP, LOGGER
 
+from ising.under_dev import MaxCutParser
+from ising.benchmarks.parsers.Knapsack import QKP_parser
+from ising.generators.Knapsack import knapsack
 from ising.benchmarks.parsers.TSP import TSP_parser
 from ising.generators.TSP import TSP
 
-save_path = TOP / "ising/under_dev/Flipping"
+save_path = TOP / "ising/under_dev/Flipping/9M_results"
 
-def run_benchmark(benchmark_name:str)->None:
+def run_benchmark_TSP(benchmark_name:str)->None:
     graph, _ = TSP_parser(TOP / f"ising/benchmarks/TSP/{benchmark_name}.tsp")
     model = TSP(graph, 1.2)
+    hyperparameters = {"dt": 1e-6, "num_iterations": 50000, "init_size":7/8, "end_size":1/12, "threshold":0.8, "cluster_choice":"gradient"}
+    run_benchmark(model, benchmark_name, **hyperparameters)
 
+def run_benchmark_MCP(benchmark_name:str)->None:
+    g, _ = MaxCutParser.G_parser(TOP / f"ising/benchmarks/G/{benchmark_name}.txt")
+    model = MaxCutParser.generate_maxcut(g)
+    hyperparameters = {"dt": 1e-4, "num_iterations": 100000, "init_size":1, "end_size":1/50, "threshold":0.7, "cluster_choice":"random"}
+    run_benchmark(model, benchmark_name, **hyperparameters)
+
+def run_benchmark_QKP(benchmark_name:str)->None:
+    profit, weight, capacity, _ = QKP_parser(TOP / f"ising/benchmarks/Knapsack/{benchmark_name}.txt")
+    model = knapsack(profit, capacity, weight, 1.2)
+    hyperparameters = {"dt": 1e-6, "num_iterations": 50000, "init_size":1/2, "end_size":1/100, "threshold":0.7, "cluster_choice":"gradient"}
+    run_benchmark(model, benchmark_name, **hyperparameters)
+
+
+def run_benchmark(model: IsingModel, benchmark_name:str, **hyperparameters) -> None:
     nb_runs = 100
-    threshold = 0.7
-    init_size = int(7/8*model.num_variables)
-    end_size = int(1/12*model.num_variables)
-    nb_flipping = 100
-    dt = 1e-6
-    num_iterations = 50000
+    threshold = hyperparameters["threshold"]
+    init_size = int(hyperparameters["init_size"]*model.num_variables)
+    end_size = int(hyperparameters["end_size"]*model.num_variables)
+    nb_flipping = 130
     sigma = np.random.choice([-1, 1], size=(model.num_variables, nb_runs))
 
     tasks = [(init_size, end_size, sigma[:, i], threshold) for i in range(nb_runs)]
-    with mp.Pool(int(mp.cpu_count()/3)) as pool:
+    with mp.Pool(initializer=os.nice, initargs=(1,)) as pool:
         flipping_partial = partial(do_flipping, model=model, 
-                                   nb_flipping=nb_flipping, 
-                                   dt=dt, 
-                                   num_iterations=num_iterations,
-                                   cluster_choice="random")
+                                   nb_flipping=nb_flipping,
+                                   dt=hyperparameters["dt"],
+                                   num_iterations=hyperparameters["num_iterations"],
+                                   cluster_choice=hyperparameters["cluster_choice"],)
         results = pool.starmap(flipping_partial, tasks)
-    # for run in range(nb_runs):
-    #     LOGGER.info(f"RUN {run + 1}/{nb_runs}")
-    #     _, best_energy, _ = do_flipping(model, sigma, init_size, end_size, nb_flipping, dt, num_iterations, threshold)
-    #     all_energies.append(best_energy)
+
     all_energies = np.array([result[1] for result in results])
     summary = np.array([[np.min(all_energies),np.max(all_energies),np.mean(all_energies),np.std(all_energies)]])
     header = "min max avg std"
-    np.savetxt(save_path / f"{benchmark_name}_summary_random_flipping.csv", summary, fmt="%.2f", header=header)
+    np.savetxt(save_path / f"{benchmark_name}_summary_{hyperparameters["cluster_choice"]}_flipping.csv", summary, fmt="%.2f", header=header)
+    np.savetxt(save_path / f"{benchmark_name}_energies_{hyperparameters["cluster_choice"]}_flipping.pkl", all_energies, fmt="%.2f")
+
+def parse_out_file(filename: str, save_file_name:str):
+    results = []
+    with open(filename, 'r') as file:
+        for line in file:
+            stripped_line = line.split(" ")
+            if stripped_line[0] == "INFO:Threshold:":
+                results.append(float(stripped_line[-1][:-2]))
+    np.savetxt(save_path / f"{save_file_name}.pkl", results)
 
 if __name__ == "__main__":
-    # run_benchmark("burma14")
-    # run_benchmark("ulysses16")
-    run_benchmark("ulysses22")
+    # run_benchmark_TSP("burma14")
+    run_benchmark_MCP("K2000")
+    # run_benchmark_QKP("jeu_100_25_1")
+    # parse_out_file(TOP / "ising/under_dev/Flipping/burma14_comparison_random.out", "TSP_random_flipping")

ising/under_dev/Flipping/new_strategy.py
@@ -9,20 +9,22 @@ from ising.solvers.Multiplicative import Multiplicative
 from ising.flow import TOP, LOGGER
 from ising.benchmarks.parsers.TSP import TSP_parser
 from ising.generators.TSP import TSP
+from ising.under_dev import MaxCutParser
+# from ising.benchmarks.parsers.Knapsack import QKP_parser
+# from ising.generators.Knapsack import knapsack
 from ising.stages.model.ising import IsingModel
 from ising.utils.flow import return_rx
 from ising.under_dev.Flipping.Wolff_cluster import find_cluster_Wolff
-from ising.under_dev.Flipping.gradient_cluster import find_cluster_gradient
+from ising.under_dev.Flipping.gradient_cluster import find_cluster_gradient, find_cluster_gradient_largest
 from ising.under_dev.Flipping.mean_cluster import find_cluster_mean
-from ising.under_dev.Flipping.frozen_gradient_cluster import find_frozen_gradient_cluster
+from ising.under_dev.Flipping.frozen_gradient_cluster import find_frozen_gradient_cluster, approximate_frozen_gradient_cluster
 from ising.under_dev.Flipping.plotting import plot_data, make_bar_plot
 np.random.seed(1)
-NICENESS = 1
+NICENESS = 0
 
 def do_flipping(cluster_size_init:int, cluster_size_end:int, sigma_init:np.ndarray,cluster_threshold:float, model:IsingModel, nb_flipping:int, dt:float, num_iterations:int, cluster_choice:str=""):
     sigma = sigma_init.copy()
     energies = []
-    nb_seeds = 2
     size_func = lambda x: int((return_rx(nb_flipping, cluster_size_init, cluster_size_end)**(x*3)) * (cluster_size_init-cluster_size_end) + cluster_size_end)
 
     prev_energy = np.inf
@@ -34,36 +36,38 @@ def do_flipping(cluster_size_init:int, cluster_size_end:int, sigma_init:np.ndarr
         if energy < prev_energy:
             prev_energy = energy
             prev_sigma = sigma.copy()
+            # LOGGER.info(f"Current best energy: {prev_energy}")
         
         cluster_size = size_func(i)
+        # LOGGER.info(f"Amount of seeds: {int(cluster_size / cluster_size_end)}")
         if cluster_choice=="random":
             cluster = np.random.choice(model.num_variables, size=(cluster_size,), replace=False)
         else:
-            cluster = find_frozen_gradient_cluster(model, prev_sigma, cluster_threshold, nb_seeds, cluster_size)
+            cluster = find_cluster_gradient(model, prev_sigma, cluster_size, cluster_threshold)
         sigma = prev_sigma.copy()
-        sigma[cluster] = -1*sigma[cluster]
-    LOGGER.info(f"Threshold: {cluster_threshold}, Best energy: {prev_energy}")
+        sigma[cluster] *= -1
+    LOGGER.info(f"Init cluster size: {cluster_size_init}, final cluster size: {cluster_size_end}, Best energy: {prev_energy}")
     return energies, prev_energy, prev_sigma
 
 
-
 def TSP_flipping():
-    burma14_graph, best_found = TSP_parser(TOP / "ising/benchmarks/TSP/burma14.tsp")
-    model_burma14 = TSP(burma14_graph, 1.2)
+    graph, best_found = MaxCutParser.G_parser(TOP / "ising/benchmarks/G/K2000.txt")
+    model_burma14 = MaxCutParser.generate_maxcut(graph)
     LOGGER.info(f"Best found: {best_found}")
 
-    dt = 1e-6
+    dt = 1e-4
     num_iterations = 50000
-    nb_runs = 20
+    nb_runs = 10
 
     sigma = np.random.uniform(-1, 1, (model_burma14.num_variables,nb_runs))
-    final_cluster_size = int(model_burma14.num_variables*1/14)
-    init_cluster_size = int(model_burma14.num_variables*7/8)
+    final_cluster_size = int(model_burma14.num_variables*1/50)
+    init_cluster_size = int(model_burma14.num_variables)
     flipping_length = 130
-    mean_cluster_th = [0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-    str_thresholds = ["0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1.0"]
+    # threshold = 0.8
+    mean_cluster_th = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    str_cluster = ["0.5", "0.6", "0.7", "0.8", "0.9", "1.0"]
     # do_flipping(cluster_size_init=init_cluster_size, cluster_size_end=final_cluster_size, sigma_init=sigma[:,0], cluster_threshold=mean_cluster_th, model=model_burma14, nb_flipping=flipping_length, dt=dt, num_iterations=num_iterations)
-    tasks = [(init_cluster_size, final_cluster_size, sigma[:,j], mean_cluster_th[i]) for i in range(len(mean_cluster_th)) for j in range(nb_runs)]
+    tasks = [(init_cluster_size, final_cluster_size, sigma[:,j], threshold) for j in range(nb_runs) for threshold in mean_cluster_th]
     with multiprocessing.Pool(initializer=os.nice, initargs=(NICENESS,)) as pool:
         flipping_partial = partial(do_flipping, model=model_burma14, 
                                                 nb_flipping=flipping_length, 
@@ -71,88 +75,11 @@ def TSP_flipping():
                                                 num_iterations=num_iterations)
         results = pool.starmap(flipping_partial, tasks)
 
-    all_energies = np.array([result[0] for result in results]).reshape((len(mean_cluster_th), nb_runs, flipping_length))
 
-    # for i in range(len(mean_cluster_th)):
-    #     mean_energies = np.mean(all_energies[i, :, :], axis=0)
-    #     max_energies = np.max(all_energies[i, :, :], axis=0)
-    #     min_energies = np.min(all_energies[i, :, :], axis=0)
-    #     std_energies = np.std(all_energies[i, :, :], axis=0)
-    #     file = TOP / f"ising/under_dev/Flipping/threshold_{mean_cluster_th[i]}_burma14.csv"
-    #     header = "mean min max std"
-    #     np.savetxt(file, np.array([mean_energies, min_energies, max_energies, std_energies]).T, fmt="%.2f", header=header)
+    best_energies = np.array([result[1] for result in results]).reshape((len(mean_cluster_th),nb_runs))
+    best_energies = np.mean(best_energies, axis=1).reshape((len(mean_cluster_th),1))
+    plot_data(best_energies, "MCP_cluster_size.png", "Threshold", str_cluster, "Start cluster size", "1", best_found)
 
-    best_energies = np.array([result[1] for result in results]).reshape((len(mean_cluster_th), nb_runs))
-    best_energies = np.mean(best_energies, axis=1).reshape((1, -1))
-    plot_data(best_energies, "burma14_cluster_threshold.png", "Threshold", str_thresholds, "Final cluster size", ["1/18"], best_found)
-
-    # dfs = {f"threshold {mean_cluster_th[i]}": None for i in range(len(mean_cluster_th))}
-    # for i, threshold in enumerate(mean_cluster_th):
-    #     dfs[f"threshold {threshold}"] = pd.DataFrame({iteration: all_energies[i, :, iteration] for iteration in range(flipping_length)})
-    # make_bar_plot(dfs, "Iteration", "Energy", "burma14_threshold_barplot.png", best_found)
-
-def TSP_flipping_more_annealing():
-    burma14_graph, best_found = TSP_parser(TOP / "ising/benchmarks/TSP/burma14.tsp")
-    model_burma14 = TSP(burma14_graph, 1.2)
-    LOGGER.info(f"Best found: {best_found}")
-
-    dt = 1e-6
-    num_iterations = 50000
-    nb_runs = 20
-
-    sigma = np.random.uniform(-1, 1, (model_burma14.num_variables,nb_runs))
-    final_cluster_size = int(model_burma14.num_variables*1/18)
-    init_cluster_size = int(model_burma14.num_variables*2/3)
-    flipping_lengths = [10, 130] 
-    threshold = 1.0
-
-    tasks = [(flipping_length, sigma[:, j]) for flipping_length in flipping_lengths for j in range(nb_runs)]
-    with multiprocessing.Pool(initializer=os.nice, initargs=(NICENESS,)) as pool:
-        flipping_partial = partial(do_multiple_annealing, model=model_burma14, 
-                                                cluster_size_init=init_cluster_size, 
-                                                cluster_size_end=final_cluster_size,
-                                                threshold=threshold, 
-                                                dt=dt, 
-                                                num_iterations=num_iterations)
-        results = pool.starmap(flipping_partial, tasks)
-    
-    dfs = {f"#flipping {flipping_lengths[i]}": None for i in range(len(flipping_lengths))}
-    all_energies = np.array([result[0] for result in results]).reshape((len(flipping_lengths), nb_runs, 100))
-    for i, length in enumerate(flipping_lengths):
-        dfs[f"#flipping {length}"] = pd.DataFrame({iteration: all_energies[i, :, iteration] for iteration in range(100)})
-    make_bar_plot(dfs, "Iteration", "Energy", "burma14_annealing_barplot.png", best_found)
-
-    
-def do_multiple_annealing(nb_flipping, sigma_init, model, cluster_size_init, cluster_size_end, dt, num_iterations, threshold):
-    if nb_flipping == 10:
-        energies = []
-        prev_energies = []
-        for i in range(13):
-            energies_current, prev_energy, prev_sigma = do_flipping(cluster_size_init=cluster_size_init, 
-                                                                    cluster_size_end=cluster_size_end, 
-                                                                    cluster_threshold=threshold, 
-                                                                    sigma_init=sigma_init, 
-                                                                    model=model, 
-                                                                    nb_flipping=nb_flipping, 
-                                                                    dt=dt, 
-                                                                    num_iterations=num_iterations)
-            energies.append(energies_current)
-            prev_energies.append(prev_energy)
-            sigma_init = prev_sigma.copy()
-        prev_energy = np.min(prev_energies)
-        LOGGER.info(f"flipping 10: energies: {energies}")
-    else:
-        energies, prev_energy, prev_sigma = do_flipping(cluster_size_init=cluster_size_init, 
-                                                        cluster_size_end=cluster_size_end, 
-                                                        cluster_threshold=threshold, 
-                                                        sigma_init=sigma_init, 
-                                                        model=model, 
-                                                        nb_flipping=nb_flipping, 
-                                                        dt=dt, 
-                                                        num_iterations=num_iterations)
-        LOGGER.info(f"flipping 100: energies: {energies}")
-    return energies, prev_energy, prev_sigma
 
 if __name__ == "__main__":
     TSP_flipping()
-    # TSP_flipping_more_annealing()