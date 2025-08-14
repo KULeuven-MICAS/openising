29e811edfe442e645134d160d72dde2b4ebe50f0
Sofie De Weer <sofie.deweer@esat.kuleuven.be>
2025-08-14 15:53:38 +0200

    fix: better handling of no best found in plot_energies

--- ising/postprocessing/energy_plot.py
+++ ising/postprocessing/energy_plot.py
@@ -55,7 +55,7 @@ def plot_energies(
     plt.xlabel("iteration")
     plt.ylabel("Energy")
     if save:
-        plt.savefig(save_folder / f"{figName}.pdf")
+        plt.savefig(save_folder / f"{figName}.png")
     plt.close()
 
 
@@ -111,7 +111,7 @@ def plot_energies_multiple(
     if best_found is not None:
         plt.ylim((best_found - 0.2*np.abs(best_found), max_energy + 0.1*np.abs(best_found)))
     if save:
-        plt.savefig(save_folder / f"{figName}.pdf", dpi=600, bbox_inches="tight")
+        plt.savefig(save_folder / f"{figName}.pdf", bbox_inches="tight")
     plt.close()
 
 
