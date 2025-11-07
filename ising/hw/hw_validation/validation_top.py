f4587b4943b10d12fd36f6eace7bcaf85cd8c965
jiacong sun <jiacong.sun@esat.kuleuven.be>
2025-11-07 22:47:13 +0100

    fix validation top

--- ising/hw/hw_validation/validation_top.py
+++ ising/hw/hw_validation/validation_top.py
@@ -1,9 +1,9 @@
 import logging
 import matplotlib.pyplot as plt
 import os
-from api import plot_results_in_bar_chart, plot_results_in_bar_chart_prim_caefa
+from api import plot_results_in_bar_chart, plot_results_in_bar_chart_with_breakdown
 from sachi import validation_to_sachi
-from prim_caefa import plot_results_in_bar_chart_with_breakdown
+from prim_caefa import validation_to_prim_caefa
 from fpga_asb_v2 import validation_to_fpga_asb_v2
 from fpga_asb import validation_to_fpga_asb
 
@@ -19,40 +19,41 @@ if __name__ == "__main__":
     )
     logging.basicConfig(level=logging_level, format=logging_format)
 
-    validation_list = ["prim_caefa", "sachi", "fpga_asb_v2", "fpga_asb_v1"]
+    validation_list = ["sachi", "fpga_asb_v2", "fpga_asb_v1", "prim_caefa"]
 
-    if os.path.exists("output") is False:
-        os.makedirs("output")
+    if os.path.exists("outputs") is False:
+        os.makedirs("outputs")
 
     for validation in validation_list:
         if validation == "sachi":
             benchmark_dict = validation_to_sachi()
-            plot_results_in_bar_chart(
-                benchmark_dict, output_file="output/sachi.svg", text_type="absolute"
+            plot_results_in_bar_chart_with_breakdown(
+                benchmark_dict, output_file="outputs/sachi.svg", text_type="absolute",
+                latency_normalize=False
             )
         elif validation == "prim_caefa":
-            benchmark_dict = plot_results_in_bar_chart_with_breakdown()
-            plot_results_in_bar_chart_prim_caefa(
+            benchmark_dict = validation_to_prim_caefa()
+            plot_results_in_bar_chart_with_breakdown(
                 benchmark_dict,
-                output_file="output/prim_caefa.svg",
+                output_file="outputs/prim_caefa.svg",
                 text_type="relative",
                 with_latency_breakdown=True,
                 latency_normalize=True
             )
         elif validation == "fpga_asb_v2":
             benchmark_dict = validation_to_fpga_asb_v2()
-            plot_results_in_bar_chart_prim_caefa(
+            plot_results_in_bar_chart_with_breakdown(
                 benchmark_dict,
-                output_file="output/fpga_asb_v2.svg",
+                output_file="outputs/fpga_asb_v2.svg",
                 text_type="absolute",
                 with_latency_breakdown=True,
                 latency_normalize=False
             )
         elif validation == "fpga_asb_v1":
             benchmark_dict = validation_to_fpga_asb()
-            plot_results_in_bar_chart_prim_caefa(
+            plot_results_in_bar_chart_with_breakdown(
                 benchmark_dict,
-                output_file="output/fpga_asb_v1.svg",
+                output_file="outputs/fpga_asb_v1.svg",
                 text_type="absolute",
                 with_latency_breakdown=True,
                 latency_normalize=False
